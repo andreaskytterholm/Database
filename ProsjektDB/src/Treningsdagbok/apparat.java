9440afff717b35efdf038bfc34af46af8da16d6e
andreaskytterholm <andreaskytterholm@dhcp-10-22-71-236.wlan.ntnu.no>
2018-03-09 15:02:46 +0100

    initial

---
+++ ProsjektDB/src/Treningsdagbok/apparat.java
@@ -0,0 +1,15 @@
+package Treningsdagbok;
+
+public class apparat {
+	
+	private int apparatID;
+	private String navn;
+	private String beskrivelse;
+	private treningssenter treningssenter;
+	
+
+}
+
+
+
+
