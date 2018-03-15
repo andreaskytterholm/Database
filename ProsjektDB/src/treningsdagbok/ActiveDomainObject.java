c6e1820a3512741279b713c08f4d1a4f7e2000e2
randi <randi@DESKTOP-UNFQVSP>
2018-03-15 10:29:52 +0100

    Fix path, naa skal det vaere litt mindre error

--- ProsjektDB/src/treningsdagbok/ActiveDomainObject.java
+++ ProsjektDB/src/treningsdagbok/ActiveDomainObject.java
@@ -2,8 +2,6 @@ package treningsdagbok;
 
 import java.sql.*;
 
-import com.mysql.jdbc.Connection;
-
 public abstract class ActiveDomainObject {
     public abstract void initialize (Connection conn);
     public abstract void refresh (Connection conn);
